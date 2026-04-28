a84624af7898ed056d2319c5d55c30fde940cda3
ademkaili <kailiadem@icloud.com>
2026-04-28 16:25:32 +0200

    changement 2 bdd

--- resources/bdd/gsb_restore.sql
+++ resources/bdd/gsb_restore.sql
@@ -15,6 +15,16 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+CREATE DATABASE IF NOT EXISTS gsb_frais
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci;
+CREATE USER IF NOT EXISTS 'userGsb'@'localhost' IDENTIFIED BY 'secret';
+GRANT SHOW DATABASES ON *.* TO 'userGsb'@'localhost';
+GRANT ALL PRIVILEGES ON `gsb_frais`.* TO userGSB@localhost;
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+USE gsb_frais;
+
 --
 -- Table structure for table `comptable`
 --
